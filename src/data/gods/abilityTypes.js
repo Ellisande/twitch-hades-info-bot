f379601612673211fbd7b8dfefdf23efb000374c
Ellisande <justin.dragos@gmail.com>
2019-07-27 23:49:15 -0700

    Add wrath as an ability type

--- src/data/gods/abilityTypes.js
+++ src/data/gods/abilityTypes.js
@@ -4,5 +4,6 @@ const CAST = "Cast";
 const DASH = "Dash";
 const REVENGE = "Revenge";
 const OTHER = "Other";
+const WRATH = "Wrath";
 
-module.exports = { ATTACK, SPECIAL, CAST, DASH, REVENGE, OTHER };
+module.exports = { ATTACK, SPECIAL, CAST, DASH, REVENGE, OTHER, WRATH };
