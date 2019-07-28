b4e5a3f030ffd229821d1c116d801b127695c484
Ellisande <justin.dragos@gmail.com>
2019-07-28 00:23:21 -0700

    Update ares to the speedy update

--- src/data/gods/ares.js
+++ src/data/gods/ares.js
@@ -1,4 +1,4 @@
-const { COMMON, RARE, EPIC, LEGENDARY } = require("./rarities");
+const { COMMON, RARE, EPIC, LEGENDARY, HEROIC, DUO } = require("./rarities");
 const { abilityFormatter } = require("./formatters");
 const {
   ATTACK,
@@ -6,9 +6,10 @@ const {
   CAST,
   DASH,
   REVENGE,
-  OTHER
+  OTHER,
+  WRATH
 } = require("./abilityTypes");
-const { mapValues, omit } = require("lodash");
+const { mapValues, toArray } = require("lodash");
 
 const info =
   "Ares, God of War. His powers cause spinning clouds of blades or damaging curses.";
@@ -16,10 +17,20 @@ const info =
 const attack = {
   name: "Curse of Agony",
   type: ATTACK,
-  info: value => `Attacks deal ${value} damage a short time later`,
+  info: value =>
+    `Your Attack inflicts Doom dealing ${value} damage after 1.1 seconds`,
   values: {
+    [COMMON]: {
+      1: 50
+    },
+    [RAREE]: {
+      1: `${50 * 1.3}-${50 * 1.5}`
+    },
     [EPIC]: {
-      1: "85"
+      1: `${50 * 1.8}-${50 * 2}`
+    },
+    [HEROIC]: {
+      1: `${50 * 2.3}-${50 * 2.5}`
     }
   }
 };
@@ -27,41 +38,54 @@ const attack = {
 const special = {
   name: "Curse of Pain",
   type: SPECIAL,
-  info: value => `Special deals ${value} damage a short time later`,
+  info: value =>
+    `Your Special inflicts Doom dealing ${value} damage after 1.2 seconds`,
   values: {
+    [COMMON]: {
+      1: 70
+    },
     [RARE]: {
-      1: "43"
+      1: `${70 * 1.3}-${70 * 1.5}`
     },
     [EPIC]: {
-      1: "73"
+      1: `${70 * 1.8}-${70 * 2}`
+    },
+    [HEROIC]: {
+      1: `${70 * 2.3}-${70 * 2.5}`
     }
   }
 };
 
 const dash = {
-  name: "Unknown",
+  name: "Blade Dash",
   type: DASH,
-  info: value => `Unknown`,
+  info: value =>
+    `Your Dash creates a Blade Rift where you started dealing ${value} damage per 0.1 seconds for 0.7 seconds`,
   values: {
-    [RARE]: {
-      1: "1sec/2crits",
-      2: "2sec/3crits"
-    },
-    [EPIC]: {
-      1: "1sec/3crits",
-      2: "2sec/4crits",
-      3: "3sec/5crits"
-    }
+    [COMMON]: { 1: 5 },
+    [RARE]: { 1: `${5 * 1.3}-${5 * 1.5}` },
+    [EPIC]: { 1: `${5 * 1.8}-${5 * 2}` },
+    [HEROIC]: { 1: `${5 * 2.3}-${5 * 2.5}` }
   }
 };
 
 const cast = {
-  name: "Unknown",
+  name: "Slicing Shot",
   type: CAST,
-  info: value => `Unknown`,
+  info: value =>
+    `Your Cast sends a Blade Rift hurling ahead dealing ${value} damage per 0.1 seconds for 4 seconds`,
   values: {
+    [COMMON]: {
+      1: 10
+    },
+    [RARE]: {
+      1: 11
+    },
     [EPIC]: {
-      1: "149-179"
+      1: 13
+    },
+    [HEROIC]: {
+      1: 14
     }
   }
 };
@@ -70,25 +94,123 @@ const revenge = {
   name: "Curse of Vengeance",
   type: REVENGE,
   info: value =>
-    `Taking damage causes nearby enemies to take ${value} damage a short time later`,
+    `After you take damage, inflict Doom on surrounding foes dealing ${value} damage after 1.1 seconds`,
   values: {
-    [EPIC]: { 1: "140" }
+    [COMMON]: { 1: 60 },
+    [RARE]: { 1: `${60 * 1.3}-${60 * 1.5}` },
+    [EPIC]: { 1: `${60 * 1.8}-${60 * 2}` }
   }
 };
 
 const urgeToKill = {
   name: "Urge to Kill",
   type: OTHER,
-  info: value => `Attack and cast damage increased by ${value}`,
+  info: value => `Your Attack and Cast deal ${value} more damage`,
   values: {
-    [EPIC]: { 1: "24%", 2: "29%", 3: "34%" }
+    [COMMON]: { 1: "8%" },
+    [RARE]: { 1: "10%" },
+    [EPIC]: { 1: "12%" }
   }
 };
 
-const base = {
-  name: "Ares",
-  info,
-  other: [urgeToKill]
+const blackMetal = {
+  name: "Black Metal",
+  type: OTHER,
+  info: value => `Your Blade Rift powers deal damage in a ${value} wider area`,
+  values: {
+    [COMMON]: { 1: "20%" },
+    [RARE]: { 1: "22%" },
+    [EPIC]: { 1: "24%" }
+  }
+};
+
+const engulfingVortex = {
+  name: "Engulfing Vortex",
+  type: OTHER,
+  info: value =>
+    `Your Blade Rift effects lasts ${value} seconds longer and pulls foes in`,
+  values: {
+    [COMMON]: { 1: "0.2" },
+    [RARE]: { 1: `${0.2 * 1.3}-${0.2 * 1.5}` },
+    [EPIC]: { 1: `${0.2 * 2}-${0.2 * 2.5}` }
+  }
+};
+
+const direMisfortune = {
+  name: "Dire Misfortune",
+  type: OTHER,
+  info: value =>
+    `Your Doom effects deal ${value} damage per application when applied multiple times`,
+  values: {
+    [COMMON]: { 1: 5 },
+    [RARE]: { 1: 7 },
+    [EPIC]: { 1: 10 }
+  }
+};
+
+const battleRage = {
+  name: "Battle Rage",
+  type: OTHER,
+  info: value =>
+    `After slaying a foe,  your next Attack or Special deals ${value} more damage`,
+  values: {
+    [COMMON]: { 1: "100%" },
+    [RARE]: { 1: "??" },
+    [EPIC]: { 1: "??" }
+  }
+};
+
+const warGodsThirst = {
+  name: "War Gods Thirst",
+  type: WRATH,
+  info: value =>
+    `Your Wrath turns you into an Invulnerable Blade Rift for 5 seconds dealing ${value} damage per hit`,
+  values: {
+    [COMMON]: { 1: 15 },
+    [RARE]: { 1: 18 },
+    [EPIC]: { 1: 21 },
+    [HEROIC]: { 1: 24 }
+  }
+};
+
+const viciousCycle = {
+  name: "Vicious Cycle",
+  type: OTHER,
+  info: value =>
+    `Your Blade Rift effects deal more ${value} damage for each consecutive hit`,
+  values: {
+    [LEGENDARY]: { 1: 2 }
+  }
+};
+
+const curseOfLonging = {
+  name: "Curse of Longing",
+  type: OTHER,
+  info: value =>
+    `Your Doom effects continuously strike Weak foes for ${value} damage`,
+  values: {
+    [DUO]: { 1: "25%" }
+  }
+};
+
+const huntingBlades = {
+  name: "Hunting Blades",
+  type: OTHER,
+  info: value =>
+    `Your Cast creates a faster Blade Rift that seeks the nearest foe for ${value} seconds`,
+  values: {
+    [DUO]: { 1: "??" }
+  }
+};
+
+const mericfulEnd = {
+  name: "Merciful End",
+  type: OTHER,
+  info: value =>
+    `Your attacks that Deflect immediately active Doom effects for ${value} damage`,
+  values: {
+    [DUO]: { 1: 120 }
+  }
 };
 
 const abilities = {
@@ -97,7 +219,22 @@ const abilities = {
   dash,
   revenge,
   cast,
-  "urge to kill": urgeToKill
+  "urge to kill": urgeToKill,
+  "black metal": blackMetal,
+  "engulfing vortex": engulfingVortex,
+  "dire mistfortune": direMisfortune,
+  "battle rage": battleRage,
+  "war gods thirst": warGodsThirst,
+  "vicious cycle": viciousCycle,
+  "curse of longing": curseOfLonging,
+  "hunting blades": huntingBlades,
+  "mericful end": mericfulEnd
+};
+
+const base = {
+  name: "Ares",
+  info,
+  other: toArray(abilities).filter(ability => ability.type === OTHER)
 };
 
 const formattedAbilities = mapValues(abilities, abilityFormatter);
