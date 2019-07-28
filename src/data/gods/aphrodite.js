6dea7556545fa5b0b16d4b1102d788fc9fcd5b7b
Ellisande <justin.dragos@gmail.com>
2019-07-27 23:49:44 -0700

    Update Aphrodite for the Speedy Update

--- src/data/gods/aphrodite.js
+++ src/data/gods/aphrodite.js
@@ -1,4 +1,4 @@
-const { COMMON, RARE, EPIC, LEGENDARY } = require("./rarities");
+const { COMMON, RARE, EPIC, LEGENDARY, HEROIC, DUO } = require("./rarities");
 const { abilityFormatter } = require("./formatters");
 const {
   ATTACK,
@@ -6,56 +6,70 @@ const {
   CAST,
   DASH,
   REVENGE,
-  OTHER
+  OTHER,
+  WRATH
 } = require("./abilityTypes");
-const { mapValues, omit } = require("lodash");
+const { mapValues, toArray } = require("lodash");
 
 const info =
   "Aphrodite, Goddess of Love. Her powers weaken enemies causing them to do less damage";
 
 const attack = {
-  name: "Unknown",
+  name: "Heartbreak Strike",
   type: ATTACK,
-  info: value => `Unknown`,
+  info: value => `Your Attack deals ${value} for damage and inflicts weak`,
   values: {
-    [RARE]: {
+    [COMON]: {
       1: "45%"
     },
+    [RARE]: {
+      1: "58.5-67.5%"
+    },
     [EPIC]: {
-      1: "66%",
-      2: "81%",
-      3: "96%",
-      4: "111%",
-      5: "126%",
-      6: "141%"
-    }
+      1: "81-90%"
+    },
+    [HEROIC]: { 1: "103.5-112.5%" }
   }
 };
 
 const special = {
-  name: "Unknown",
+  name: "Heartbreak Flourish",
   type: SPECIAL,
-  info: value => `Unknown`,
+  info: value =>
+    `Your Special deals more ${value} more damage and inflicts weak`,
   values: {
     [COMMON]: {
-      1: "25%"
+      1: "80%"
     },
     [RARE]: {
-      1: "35%",
-      2: "48%",
-      3: "61%",
-      4: "74%"
+      1: `${80 * 1.3}-${80 * 1.5}%`
+    },
+    [EPIC]: {
+      1: `${80 * 1.8}-${80 * 2.0}%`
+    },
+    [HEROIC]: {
+      1: `${80 * 2.3}-${80 * 2.5}%`
     }
   }
 };
 
 const dash = {
-  name: "Unknown",
+  name: "Passion Dash",
   type: DASH,
-  info: value => `Unknown`,
+  info: value =>
+    `Your Dash deals ${value} damage at the start and end, and inflicts Weak`,
   values: {
+    [COMMON]: {
+      1: "17-19"
+    },
     [RARE]: {
-      1: 32
+      1: `${17 * 1.3}-${19 * 1.5}`
+    },
+    [EPIC]: {
+      1: `${17 * 1.8}-${19 * 2.0}`
+    },
+    [HEROIC]: {
+      1: `${17 * 2.3}-${19 * 2.5}`
     }
   }
 };
@@ -64,11 +78,19 @@ const cast = {
   name: "Shatter Shot",
   type: CAST,
   info: value =>
-    `Cast shoots six short range projectiles that each deal ${value} damage`,
+    `Your Cast fires in a reduced-range spread pattern that deals ${value} damage and inflicts Weak`,
   values: {
     [COMMON]: {
-      1: "15",
-      2: "23"
+      1: 90
+    },
+    [RARE]: {
+      1: 108
+    },
+    [EPIC]: {
+      1: 126
+    },
+    [HEROIC]: {
+      1: 144
     }
   }
 };
@@ -77,12 +99,13 @@ const revenge = {
   name: "Wave of Despair",
   type: REVENGE,
   info: value =>
-    `Taking damage deals ${value} damage to nearby enemies and applies weak`,
+    `After you take damage, damage nearby fores for ${value} and turn them Weak`,
   values: {
     [COMMON]: {
-      1: "14"
+      1: "20-25"
     },
-    [RARE]: { 1: "17", 2: "23" }
+    [RARE]: { 1: `${20 * 1.3}-${25 * 1.5}` },
+    [EPIC]: { 1: `${20 * 2}-${20 * 2.2}` }
   }
 };
 
@@ -90,27 +113,90 @@ const dyingLament = {
   name: "Dying Lament",
   type: OTHER,
   info: value =>
-    `Killing an  enemy deals ${value} damage to nearby enemies and applies weak`,
+    `When slain, foes damage other nearby foes for ${value} damage and inflict Weak`,
   values: {
-    [RARE]: { 1: "15-18" }
+    [COMMON]: { 1: "25-30" },
+    [RARE]: { 1: `${25 * 1.3}-${30 * 1.5}` },
+    [EPIC]: { 1: `${25 * 2}-${30 * 2.2}` }
   }
 };
 
 const emptyInside = {
   name: "Empty Inside",
   type: OTHER,
-  info: value => `Your weak effects last ${value} longer`,
+  info: value =>
+    `Your weak effects have a longer duration. Adds ${value} additional seconds`,
   values: {
-    [EPIC]: {
-      1: "5sec"
-    }
+    [COMMON]: { 1: 5 },
+    [RARE]: { 1: 7.5 },
+    [EPIC]: { 1: 10 }
   }
 };
 
-const base = {
-  name: "Aphrodite",
-  info,
-  other: [dyingLament, emptyInside]
+const differentLeague = {
+  name: "Different League",
+  type: OTHER,
+  info: value => `Resist ${value} damage from nearby foes' attacks`,
+  values: {
+    [COMMON]: { 1: "10%" },
+    [RARE]: { 1: "12%" },
+    [EPIC]: { 1: "15%" }
+  }
+};
+
+const sweetSurrender = {
+  name: "Sweet Surrender",
+  type: OTHER,
+  info: value =>
+    `Your Weak effects also make foes ${value} more susceptible to damage`,
+  values: {
+    [COMMON]: { 1: "10%" },
+    [RARE]: { 1: "13-15%" },
+    [EPIC]: { 1: "20-25%" }
+  }
+};
+
+const charmOfTheLovelyGoddess = {
+  name: "Charm of the Lovely Goddess",
+  type: WRATH,
+  info: value =>
+    `Your Wrath fires a seeking projectile that leaves foes Charmed for ${value} seconds`,
+  values: {
+    [COMMON]: { 1: 10 },
+    [RARE]: { 1: 11 },
+    [EPIC]: { 1: 12 },
+    [HEROIC]: { 1: 13 }
+  }
+};
+
+const unhealthyFixation = {
+  name: "Unhealthy Fixation",
+  type: OTHER,
+  info: value =>
+    `Your Weak effects also have a ${value} chance to make foes Charmed for 7 seconds`,
+  values: {
+    [LEGENDARY]: { 1: "15%" }
+  }
+};
+
+const curseOfLonging = {
+  name: "Curse of Longing",
+  type: OTHER,
+  info: value =>
+    `Your Doom effects continuously strike Weak foes for ${value} damage`,
+  values: {
+    [DUO]: { 1: "25%" }
+  }
+};
+
+const lowTolerance = {
+  name: "Low Tolerance",
+  type: OTHER,
+  info: value =>
+    `Your Blight Wine effects stack ${value} more times against Weak foes`,
+  values: {
+    [DUO]: { 1: 3 }
+  }
 };
 
 const abilities = {
@@ -119,8 +205,20 @@ const abilities = {
   dash,
   revenge,
   cast,
+  wrath: charmOfTheLovelyGoddess,
   "dying lament": dyingLament,
-  "empty inside": emptyInside
+  "empty inside": emptyInside,
+  "different league": differentLeague,
+  "sweet surrender": sweetSurrender,
+  "unhealthy fixation": unhealthyFixation,
+  "curse of longing": curseOfLonging,
+  "low tolerance": lowTolerance
+};
+
+const base = {
+  name: "Aphrodite",
+  info,
+  other: toArray(abilities).filter(ability => ability.OTHER)
 };
 
 const formattedAbilities = mapValues(abilities, abilityFormatter);
