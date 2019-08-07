b5761d597b9cd04a83bebd1c5f0c4e23f9240a2f
Ellisande <justin.dragos@gmail.com>
2019-08-06 18:57:07 -0700

    Add godlist to all commands

--- src/commands/all.js
+++ src/commands/all.js
@@ -3,6 +3,6 @@ const { weaponCommand } = require("./weapon");
 const { godCommand } = require("./god");
 const { godList } = require("./godList");
 
-const allCommands = [pingCommand, weaponCommand, godCommand];
+const allCommands = [pingCommand, weaponCommand, godCommand, godList];
 
 module.exports = { allCommands };
