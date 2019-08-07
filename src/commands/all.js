dff37c04b8569e21235b33ca35c13c9a70afecf5
Ellisande <justin.dragos@gmail.com>
2019-08-06 18:59:20 -0700

    Fixed a named import typo

--- src/commands/all.js
+++ src/commands/all.js
@@ -1,8 +1,8 @@
 const { pingCommand } = require("./ping");
 const { weaponCommand } = require("./weapon");
 const { godCommand } = require("./god");
-const { godList } = require("./godList");
+const { godListCommand } = require("./godList");
 
-const allCommands = [pingCommand, weaponCommand, godCommand, godList];
+const allCommands = [pingCommand, weaponCommand, godCommand, godListCommand];
 
 module.exports = { allCommands };
