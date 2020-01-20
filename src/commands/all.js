cbbedfa28745bee43e00f6e3a4008c9cd227a736
Ellisande <justin.dragos@gmail.com>
2020-01-19 23:52:05 -0700

    Add the two hammer commands to the command pool.

--- src/commands/all.js
+++ src/commands/all.js
@@ -3,13 +3,17 @@ const { weaponCommand } = require("./weapon");
 const { godCommand } = require("./god");
 const { godListCommand } = require("./godList");
 const { boonCommand } = require("./boon");
+const { hammerAbilityCommand } = require("./hammerAbility");
+const { hammerCommand } = require("./hammer");
 
 const allCommands = [
   pingCommand,
   weaponCommand,
   godCommand,
   godListCommand,
-  boonCommand
+  boonCommand,
+  hammerAbilityCommand,
+  hammerCommand
 ];
 
 module.exports = { allCommands };
