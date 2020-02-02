89195e9bf3f7c641c4f6044af1fbb04684af881d
Ellisande <justin.dragos@gmail.com>
2020-02-02 14:21:22 -0700

    Added several demeter abilities and updates from wiki

--- src/data/gods/demeter.js
+++ src/data/gods/demeter.js
@@ -17,46 +17,20 @@ const { mapValues, toArray } = require("lodash");
 
 const info = "Demeter, Goddess of Seasons. Her abilities slows enemies.";
 
-const attackBase = "50";
+const attackBase = "40";
 const attack = {
   name: "Frost Strike",
   type: ATTACK,
   info: value => `Your Attack is ${value}% stronger and inflicts Chill`,
-  values: {
-    [COMMON]: {
-      1: 40
-    },
-    [RARE]: {
-      1: 56
-    },
-    [EPIC]: {
-      1: 79
-    },
-    [HEROIC]: {
-      1: 0
-    }
-  }
+  values: calculatePercentage(attackBase, true)
 };
 
-const specialBase = 80;
+const specialBase = 60;
 const special = {
   name: "Frost Flourish",
   type: SPECIAL,
   info: value => `Your Special is ${value} stronger and inflicts Chill`,
-  values: {
-    [COMMON]: {
-      1: 0
-    },
-    [RARE]: {
-      1: 82
-    },
-    [EPIC]: {
-      1: 111
-    },
-    [HEROIC]: {
-      1: 0
-    }
-  }
+  values: calculatePercentage(specialBase, true)
 };
 
 const cast = {
@@ -66,22 +40,20 @@ const cast = {
     `Your Cast drops a crystal that fires a beam stright ahead for ${value} damage every 0.2s for 5s.`,
   values: {
     [COMMON]: {
-      1: 0
+      1: 8
     },
     [RARE]: {
-      1: 0
+      1: 9
     },
     [EPIC]: {
       1: 10
     },
     [HEROIC]: {
-      1: 0
+      1: 11
     }
   }
 };
 
-const dashMin = 17;
-const dashMax = 19;
 const dash = {
   name: "Mistral Dash",
   type: DASH,
@@ -89,44 +61,26 @@ const dash = {
     `Your Dash shoots a gust ahead for ${value} damage that inflicts Chill`,
   values: {
     [COMMON]: {
-      1: 0
+      1: 10
     },
     [RARE]: {
-      1: 0
+      1: 15
     },
     [EPIC]: {
       1: 20
     },
     [HEROIC]: {
-      1: 0
+      1: 25
     }
   }
 };
 
-const revengeMin = 20;
-const revengeMax = 25;
 const revenge = {
-  name: "[TODO] Revenge",
+  name: "None",
   type: REVENGE,
-  info: value => `After you take damage, [TODO]`,
+  info: () => `Demeter does not have a revenge ability.`,
   values: {
     [COMMON]: {
-      1: `${revengeMin}-${revengeMax}`
-    },
-    [RARE]: { 1: `${revengeMin * 1.3}-${revengeMax * 1.5}` },
-    [EPIC]: { 1: `${revengeMin * 2}-${(revengeMax * 2.2).toFixed(0)}` }
-  },
-  values: {
-    [COMMON]: {
-      1: 0
-    },
-    [RARE]: {
-      1: 0
-    },
-    [EPIC]: {
-      1: 0
-    },
-    [HEROIC]: {
       1: 0
     }
   }
@@ -161,16 +115,16 @@ const arcticBlast = {
     `Applying 10 stacks of Chill causes a blast for ${value} damage, clearing the effect.`,
   values: {
     [COMMON]: {
-      1: 50
-    },
-    [RARE]: {
-      1: 75
-    },
-    [EPIC]: {
       1: 100
     },
+    [RARE]: {
+      1: 120
+    },
+    [EPIC]: {
+      1: 140
+    },
     [HEROIC]: {
-      1: 0
+      1: 160
     }
   }
 };
@@ -179,10 +133,10 @@ const nourishedSoul = {
   name: "Nourished Soul",
   type: OTHER,
   info: value =>
-    `Any Healing effects are ${value}% more potent. Restore 13% health now.`,
+    `Any Healing effects are ${value}% more potent. Restore health now.`,
   values: {
     [COMMON]: {
-      1: 0
+      1: 30
     },
     [RARE]: {
       1: 32
@@ -191,7 +145,7 @@ const nourishedSoul = {
       1: 35
     },
     [HEROIC]: {
-      1: 0
+      1: 38
     }
   }
 };
@@ -203,7 +157,7 @@ const snowBurst = {
     `Whenver you Cast, damage nearby foes for ${value} and inflict Chill.`,
   values: {
     [COMMON]: {
-      1: 0
+      1: 40
     },
     [RARE]: {
       1: 50
@@ -212,7 +166,7 @@ const snowBurst = {
       1: 60
     },
     [HEROIC]: {
-      1: 0
+      1: 70
     }
   }
 };
@@ -224,16 +178,16 @@ const rareCrop = {
     `${value} of your boons randomly become common, then gain rarity every 3 encounters.`,
   values: {
     [COMMON]: {
-      1: 0
+      1: 1
     },
     [RARE]: {
-      1: 0
+      1: 2
     },
     [EPIC]: {
       1: 3
     },
     [HEROIC]: {
-      1: 0
+      1: 4
     }
   }
 };
@@ -254,7 +208,7 @@ const ravenousWill = {
       1: 30
     },
     [HEROIC]: {
-      1: 0
+      1: 40
     }
   }
 };
@@ -263,20 +217,20 @@ const ravenousWill = {
 const killingFreeze = {
   name: "Killing Freeze",
   type: OTHER,
-  info: value =>
-    `While all foes are Chill-afllicted, they are 12% slower and take ${value} decay damage every 0.5s`,
+  info: ([slow, damage]) =>
+    `While all foes are Chill-afllicted, they are ${slow} slower and take ${damage} decay damage every 0.5s`,
   values: {
     [COMMON]: {
-      1: 0
+      1: [10, 20]
     },
     [RARE]: {
-      1: 24
+      1: [15, 30]
     },
     [EPIC]: {
-      1: 28
+      1: [20, 40]
     },
     [HEROIC]: {
-      1: 0
+      1: [25, 50]
     }
   }
 };
@@ -303,6 +257,36 @@ const coldFusion = {
   }
 };
 
+const crystalClarity = {
+  name: "Crystal Clarity",
+  type: OTHER,
+  info: value =>
+    `Your Cast is ${value}% stronger and tracks foes more effectively`,
+  values: {
+    [DUO]: { 1: 10 }
+  }
+};
+
+const blizzardShot = {
+  name: "Blizzard Shot",
+  type: OTHER,
+  info: value =>
+    `Your Cast moves slowly, piercing foes and firing shards around it for ${value} damage`,
+  values: {
+    [DUO]: { 1: 40 }
+  }
+};
+
+const iceWine = {
+  name: "Ice Wine",
+  type: OTHER,
+  info: value =>
+    `After a brief delay your Cast bursts and damages nearby foes for ${value}% more damage.`,
+  values: {
+    [DUO]: { 1: 50 }
+  }
+};
+
 const abilities = {
   attack,
   special,
@@ -317,7 +301,10 @@ const abilities = {
   winterHarvest,
   ravenousWill,
   killingFreeze,
-  coldFusion
+  coldFusion,
+  crystalClarity,
+  blizzardShot,
+  iceWine
 };
 
 const base = {
