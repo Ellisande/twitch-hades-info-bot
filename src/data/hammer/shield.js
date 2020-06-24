33bd0b90c1eb48f44368340ad9dfcc8bb75d5457
Ellisande <justin.dragos@gmail.com>
2020-06-23 22:06:15 -0700

    Added 3 new hammer abilities for new shield

--- src/data/hammer/shield.js
+++ src/data/hammer/shield.js
@@ -4,57 +4,76 @@ const weaponNameString = "Shield";
 const dreadFlight = {
   name: "Dread Flight",
   description: "Your Special can hit up to 6 foes before returning.",
-  exclusives: ["zeus aspect"]
+  exclusives: ["zeus aspect"],
 };
 
 const suddenRush = {
   name: "Sudden Rush",
   description:
     "Your Bull Rush charges much faster, but does not knock foes away.",
-  exclusives: []
+  exclusives: [],
 };
 
 const pulverizingBlow = {
   name: "Pulverizing Blow",
   description: "Your Attack hits twice, but does not knock foes away.",
-  exclusives: []
+  exclusives: [],
 };
 
 const dashingWallop = {
   name: "Dashing Wallop",
   description: "Your Dash Attack deals +50% damage in a larger area.",
-  exclusives: []
+  exclusives: [],
 };
 
 const explosiveReturn = {
   name: "Explosive Return",
   description: "Your Special deals 60 damage to nearby foes when you catch it.",
-  exclusives: []
+  exclusives: [],
 };
 
 const minotaurRush = {
   name: "Minotaur Rush",
   description: "Your Bull Rush gains a Power Shot.",
-  exclusives: []
+  exclusives: [],
 };
 
 const breachingRush = {
   name: "Breaching Rush",
   description: "Your Bull Rush deals +400% damage to Armor.",
-  exclusives: []
+  exclusives: [],
 };
 
 const chargedShot = {
   name: "Charged Shot",
   description:
     "Your Bull Rush instead fires a piercing shot that deals 80 damage.",
-  exclusives: []
+  exclusives: [],
 };
 
 const chargedThrow = {
   name: "Charged Throw",
   description: "Hold Special to charge your throw for up to +300% damage.",
-  exclusives: []
+  exclusives: [],
+};
+
+const chargedFlight = {
+  name: "Charged Flight",
+  description: "Hold Special to charge your throw for up to +400% damage.",
+  exclusives: [],
+};
+
+const dashingFlight = {
+  name: "Dashing Flight",
+  description:
+    "While your Dash, your Special is faster and deals +100% damage.",
+  exclusives: [],
+};
+
+const empoweringFlight = {
+  name: "Empowering Flight",
+  description: "After your Special hits, your next 2 Attacks deal +80% damage.",
+  exclusives: [],
 };
 
 const formatter = hammerFormatter(weaponNameString);
@@ -68,9 +87,12 @@ const shieldHammerAbilities = [
   minotaurRush,
   breachingRush,
   chargedShot,
-  chargedThrow
+  chargedThrow,
+  chargedFlight,
+  dashingFlight,
+  empoweringFlight,
 ].map(formatter);
 
 module.exports = {
-  shieldHammerAbilities
+  shieldHammerAbilities,
 };
