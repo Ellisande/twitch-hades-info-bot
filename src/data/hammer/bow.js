eed6841e7c4d39e91cb58e96dfbdda54cc1e3c5b
Ellisande <justin.dragos@gmail.com>
2020-06-23 23:40:57 -0700

    Added repulse shot for the bow

--- src/data/hammer/bow.js
+++ src/data/hammer/bow.js
@@ -4,63 +4,69 @@ const weaponNameString = "Bow";
 const twinShot = {
   name: "Twin Shot",
   description: "Your Attack fires 2 shots side-by-side, but has reduced range.",
-  exclusives: ["triple shot"]
+  exclusives: ["triple shot"],
 };
 
 const sniperShot = {
   name: "Sniper Shot",
   description: "Your Attack deals +300% damage to distant foes.",
-  exclusives: []
+  exclusives: [],
 };
 
 const explosiveShot = {
   name: "Explosive Shot",
   description:
     "Your Attack deals +300% damage in an area, but chargers slower.",
-  exclusives: ["flurry shot"]
+  exclusives: ["flurry shot"],
 };
 
 const flurryShot = {
   name: "Flurry Shot",
   description: "Hold Attack to shoot rapidly, but you cannot Power Shot.",
-  exclusives: ["perfect shot", "explosive shot"]
+  exclusives: ["perfect shot", "explosive shot"],
 };
 
 const piercingBarrage = {
   name: "Piercing Barrage",
   description: "Your Special pierces foes and deals +100% damage to armor.",
-  exclusives: ["chrion aspect"]
+  exclusives: ["chrion aspect"],
 };
 
 const perfectShot = {
   name: "Perfect Shot",
   description: "Your Power Shot deals +100% damage.",
-  exclusives: ["flurry shot"]
+  exclusives: ["flurry shot"],
 };
 
 const relentlessBarrage = {
   name: "Relentless Barrage",
   description: "Your Special shoots +4 shots",
-  exclusives: []
+  exclusives: [],
 };
 
 const tripleShot = {
   name: "Triple Shot",
   description: "Your Attack fires 3 shots in a spread pattern",
-  exclusives: ["twin shot"]
+  exclusives: ["twin shot"],
 };
 
 const chainShot = {
   name: "Chain Shot",
   description:
     "Your Attack bounces to up to 3 foes, dealing +15% damage for each.",
-  exclusives: []
+  exclusives: [],
 };
 
 const chargedVolley = {
   name: "Charged Volley",
   description: "Hold Special to charge your volley for up to 300% damage",
-  exclusives: []
+  exclusives: [],
+};
+
+const repulseShot = {
+  name: "Charged Volley", //todo fix
+  description: "Your Celestial Sharanga Attack creates a Blast Wave around you",
+  exclusives: [],
 };
 
 const formatter = hammerFormatter(weaponNameString);
@@ -75,9 +81,10 @@ const bowHammerAbilities = [
   relentlessBarrage,
   tripleShot,
   chainShot,
-  chargedVolley
+  chargedVolley,
+  repulseShot,
 ].map(formatter);
 
 module.exports = {
-  bowHammerAbilities
+  bowHammerAbilities,
 };
