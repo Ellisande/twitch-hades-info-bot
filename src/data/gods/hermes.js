d0696c2ad56f15e6a28a7a32d0b07faeabdff727
Ellisande <justin.dragos@gmail.com>
2020-06-23 18:17:28 -0700

    Added side hustle

--- src/data/gods/hermes.js
+++ src/data/gods/hermes.js
@@ -7,7 +7,7 @@ const {
   DASH,
   REVENGE,
   OTHER,
-  AID
+  AID,
 } = require("./abilityTypes");
 const { mapValues, toArray } = require("lodash");
 
@@ -17,161 +17,172 @@ const info =
 const attack = {
   name: "Swift Strike",
   type: ATTACK,
-  info: value => `Your Attack is ${value} faster`,
+  info: (value) => `Your Attack is ${value} faster`,
   values: {
     [COMMON]: {
-      1: "12-13%"
+      1: "12-13%",
     },
     [RARE]: {
-      1: "21%"
+      1: "21%",
     },
     [EPIC]: {
-      1: "30%"
-    }
-  }
+      1: "30%",
+    },
+  },
 };
 
 const special = {
   name: "Swift Flourish",
   type: SPECIAL,
-  info: value => `Your Special is ${value} faster`,
+  info: (value) => `Your Special is ${value} faster`,
   values: {
     [COMMON]: {
-      1: "12-13%"
+      1: "12-13%",
     },
     [RARE]: {
-      1: "21%"
+      1: "21%",
     },
     [EPIC]: {
-      1: "30%"
-    }
-  }
+      1: "30%",
+    },
+  },
 };
 
 const cast = {
   name: "Rapid Cast",
   type: CAST,
-  info: value => `Your Cast is ${value} faster and fully automatic`,
+  info: (value) => `Your Cast is ${value} faster and fully automatic`,
   values: {
     [COMMON]: {
-      1: "20%"
+      1: "20%",
     },
     [RARE]: {
-      1: "60%"
+      1: "60%",
     },
     [EPIC]: {
-      1: "88%"
-    }
-  }
+      1: "88%",
+    },
+  },
 };
 
 const dash = {
   name: "Greatest Reflex",
   type: DASH,
-  info: value => `You can dash ${value} additional times`,
+  info: (value) => `You can dash ${value} additional times`,
   values: {
     [COMMON]: {
-      1: 15
+      1: 15,
     },
     [RARE]: {
-      1: `${15 * 1.3}-${15 * 1.5}`
+      1: `${15 * 1.3}-${15 * 1.5}`,
     },
     [EPIC]: {
-      1: `${15 * 1.8}-${15 * 2.0}`
+      1: `${15 * 1.8}-${15 * 2.0}`,
     },
     [HEROIC]: {
-      1: `${15 * 2.3}-${15 * 2.5}`
-    }
-  }
+      1: `${15 * 2.3}-${15 * 2.5}`,
+    },
+  },
 };
 
 const driftDash = {
   name: "Drift Dash",
   type: OTHER,
-  info: value => `After you Dash, gain +100% move speed for ${value} seconds`,
+  info: (value) => `After you Dash, gain +100% move speed for ${value} seconds`,
   values: {
     [COMMON]: { 1: 0.6 },
     [RARE]: { 1: 0.75 },
-    [EPIC]: { 1: 0.9 }
-  }
+    [EPIC]: { 1: 0.9 },
+  },
 };
 
 const passingThrough = {
   name: "Passing Through",
   type: OTHER,
-  info: value => `Running into foes makes then ${value} slows for 3 seconds`,
+  info: (value) => `Running into foes makes then ${value} slows for 3 seconds`,
   values: {
     [COMMON]: { 1: "20%" },
     [RARE]: { 1: "25%" },
-    [EPIC]: { 1: "30%" }
-  }
+    [EPIC]: { 1: "30%" },
+  },
 };
 
 const greaterHaste = {
   name: "Greater Haste",
   type: OTHER,
-  info: value => `Your move ${value} faster`,
+  info: (value) => `Your move ${value} faster`,
   values: {
     [COMMON]: { 1: "20%" },
     [RARE]: { 1: "30%" },
-    [EPIC]: { 1: "40%" }
-  }
+    [EPIC]: { 1: "40%" },
+  },
 };
 
 const quickRecovery = {
   name: "Quick Recovery",
   type: OTHER,
-  info: value =>
+  info: (value) =>
     `After you damage, quickly Dash to recover up to ${value} of health lost`,
   values: {
     [COMMON]: { 1: "20%" },
     [RARE]: { 1: "30%" },
-    [EPIC]: { 1: "40%" }
-  }
+    [EPIC]: { 1: "40%" },
+  },
 };
 
 const greaterEvasion = {
   name: "Greater Evasion",
   type: OTHER,
-  info: value => `Your have ${value} chance to dodge`,
+  info: (value) => `Your have ${value} chance to dodge`,
   values: {
     [COMMON]: { 1: "15%" },
     [RARE]: { 1: `20%` },
-    [EPIC]: { 1: `25%` }
-  }
+    [EPIC]: { 1: `25%` },
+  },
 };
 
 const secondWind = {
   name: "Second Wind",
   type: AID,
-  info: value =>
+  info: (value) =>
     `After using Call, gain ${value} Dodge chance and move speed for 5 Sec.`,
   values: {
     [COMMON]: { 1: "30%" },
     [RARE]: { 1: "37%" },
-    [EPIC]: { 1: "45%" }
-  }
+    [EPIC]: { 1: "45%" },
+  },
+};
+
+const sideHustle = {
+  name: "Side Hustle",
+  type: OTHER,
+  info: (value) => `Each time you enter a Chamber gain ${value} money`,
+  values: {
+    [COMMON]: { 1: "10" },
+    [RARE]: { 1: "0" },
+    [EPIC]: { 1: "0" },
+  },
 };
 
 const relativeSpeed = {
   name: "Relative Speed",
   type: OTHER,
   info: () => `Your Dash makes nearby foes 50% for 1 second`,
-  values: {}
+  values: {},
 };
 
 const greaterRecall = {
   name: "Greater Recall",
   type: OTHER,
   info: () => `Your casts are automatically returned to you`,
-  values: {}
+  values: {},
 };
 
 const revenge = {
   name: "None",
   type: REVENGE,
   info: () => `Hermes has no revenge ability`,
-  values: {}
+  values: {},
 };
 
 const abilities = {
@@ -187,21 +198,22 @@ const abilities = {
   "quick recovery": quickRecovery,
   "greater evasion": greaterEvasion,
   "relative speed": relativeSpeed,
-  "greater recall": greaterRecall
+  "greater recall": greaterRecall,
+  "side hustle": sideHustle,
 };
 
 const base = {
   name: "Hermes",
   info,
   abilities,
-  other: toArray(abilities).filter(ability => ability.type === OTHER)
+  other: toArray(abilities).filter((ability) => ability.type === OTHER),
 };
 
 const formattedAbilities = mapValues(abilities, abilityFormatter(base.name));
 
 const hermes = {
   ...base,
-  ...formattedAbilities
+  ...formattedAbilities,
 };
 
 module.exports = { hermes };
