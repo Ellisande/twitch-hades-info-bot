2f1b41d95a4eaa399ef93ce6f145ab39b9b46936
sneakyteak <sneaky.teakster@gmail.com>
2024-05-14 15:03:08 -0500

    Updates some Zeus boons
    Per in-game codex, updates some Zeus boons with new values or elements.

--- src/data/gods/zeus.ts
+++ src/data/gods/zeus.ts
@@ -1,8 +1,9 @@
 import { COMMON, RARE, EPIC, LEGENDARY, HEROIC, DUO } from "./rarities";
 import { abilityFormatter } from "./formatters";
-import { ATTACK, SPECIAL, CAST, DASH, OTHER } from "./abilityTypes";
+import { ATTACK, SPECIAL, CAST, DASH, OTHER, GAIN } from "./abilityTypes";
 import { mapValues, toArray } from "lodash";
 import { Boon, God } from "./god";
+import { AIR } from "./elements";
 
 const info =
   "Zeus, God of Thunder. His powers create bouncing lightning projectiles.";
@@ -10,8 +11,12 @@ const info =
 const attack: Boon = {
   name: "Heaven Strike",
   type: ATTACK,
+  element: AIR,
   info: (value) => `Your Attacks inflict ${value} [blitz] damage`,
   values: {
+    [COMMON]: {
+      1: 80,
+    },
     [RARE]: {
       1: 120,
     },
@@ -24,6 +29,7 @@ const attack: Boon = {
 const special: Boon = {
   name: "Heaven Flourish",
   type: SPECIAL,
+  element: AIR,
   info: (value) => `Your Specials inflict ${value} [blitz]`,
   values: {
     [COMMON]: {
@@ -37,9 +43,10 @@ const special: Boon = {
 
 const cast: Boon = {
   name: "Storm Ring",
-  type: OTHER,
+  type: CAST,
+  element: AIR,
   info: (value) =>
-    `Your omega cast also causes lightning bolts to repeatedly strike 1 foe at a time for ${value} damage`,
+    `Your [omega] Cast also causes lightning bolts to repeatedly strike 1 foe at a time for ${value} damage`,
   values: {
     [COMMON]: { 1: 30, 2: 40 },
     [RARE]: { 1: 40 },
@@ -50,6 +57,7 @@ const cast: Boon = {
 const dash: Boon = {
   name: "Thunder Sprint",
   type: DASH,
+  element: AIR,
   info: (value) =>
     `Your Sprint causes nearby foes to be struck by lightning bolts for ${value} damage, which use 3 [mana] each`,
   values: {
@@ -58,35 +66,69 @@ const dash: Boon = {
   },
 };
 
-const divineVengeance: Boon = {
-  name: "Divine Vengeance",
-  type: OTHER,
-  info: (value) =>
-    `After you take damage, your foe is stuck by lightning for ${value}, and again 50% of the time (up to 3 times)`,
-  values: {
-    [RARE]: { 1: 100 },
-  },
-};
-
-const ionicGain: Boon = {
+const gain: Boon = {
   name: "Ionic Gain",
-  type: OTHER,
+  type: GAIN,
+  element: AIR,
   info: (value) =>
     `Gradually restore [mana] by ${value}/sec, but your total amount is reduced by 70%`,
   values: {
-    [COMMON]: { 1: 6 },
+    [COMMON]: { 1: 4 },
     [RARE]: { 1: 6 },
+  },
+};
+
+// Infusion
+const airQuality: Boon = {
+  name: "Air Quality",
+  type: OTHER,
+  info: (value) =>
+    `While you have at least 5 [air], you can never deal less damage than ${value} per hit.`,
+  values: {
+    [COMMON]: { 1: 30, },
+    [RARE]: { 1: 30, },
+  },
+};
+
+const divineVengeance: Boon = {
+  name: "Divine Vengeance",
+  type: OTHER,
+  element: AIR,
+  info: (value) =>
+    `After you take damage, your foe is struck by lightning for 100 damage, and again 50% of the time (up to ${value} times)`,
+  values: {
+    [COMMON]: { 1: 2, },
+    [RARE]: { 1: 3, },
+  },
+};
+
+const lightningLance: Boon = {
+  name: "Lightning Lance",
+  type: OTHER,
+  element: AIR,
+  info: (value) =>
+    `Hold Cast to aim where the binding circle appears. Foes within are stuck by lightning for ${value} damage`,
+  values: {
+    [COMMON]: { 1: 50 },
+    [RARE]: {
+      1: "70",
+      2: "90",
+      3: "100",
+      4: "105",
+    },
   },
 };
 
 const staticShock: Boon = {
   name: "Static Shock",
   type: OTHER,
+  element: AIR,
   info: (value) =>
     `After you enter a Location, [prime] 50 [mana] to make your strikes emit chain-lightning that deals ${value} damage`,
   values: {
-    [RARE]: { 1: 15 },
-    [EPIC]: { 1: 20 },
+    [COMMON]: { 1: 10, },
+    [RARE]: { 1: 15, },
+    [EPIC]: { 1: 20, },
   },
 };
 
@@ -111,22 +153,6 @@ const spiritSurge: Boon = {
   },
 };
 
-const lightningLance: Boon = {
-  name: "Lightning Lance",
-  type: OTHER,
-  info: (value) =>
-    `Hold Cast to aim where the binding circle appears. Foes within are stuck by lightning for ${value} damage`,
-  values: {
-    [COMMON]: { 1: 50 },
-    [RARE]: {
-      1: "70",
-      2: "90",
-      3: "100",
-      4: "105",
-    },
-  },
-};
-
 const kingsRansom: Boon = {
   name: "King's Ransom",
   type: OTHER,
@@ -144,16 +170,6 @@ const electricOverload: Boon = {
     `Whenever your [blitz] effects activate, a bolt of chain-lightning fires from the foe dealing ${value} damage`,
   values: {
     [RARE]: { 1: 15 },
-  },
-};
-
-const airQuality: Boon = {
-  name: "Air Quality",
-  type: OTHER,
-  info: (value) =>
-    `While you have at least 5 [air], you can never deal less damage than the limit`,
-  values: {
-    [RARE]: { 1: "30" },
   },
 };
 
@@ -180,18 +196,17 @@ const toastingFork: Boon = {
 const abilities = {
   attack,
   special,
-  dash,
   cast,
-  lightningLance,
+  dash,
+  gain,
+  airQuality,
   divineVengeance,
-  ionicGain,
-  stormRing: cast,
+  lightningLance,
   staticShock,
   doubleStrike,
   spiritSurge,
   "king's ransom": kingsRansom,
   electricOverload,
-  airQuality,
   masterConductor,
   toastingFork,
 };
